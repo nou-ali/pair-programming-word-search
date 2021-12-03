0315b579411227eec398cc057a1989e081a96a5e
nou-ali <noura4403@gmail.com>
2021-12-03 00:03:24 +0000

    solution

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,31 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
 
-module.exports = wordSearch
+const wordSearch = (letters, word) => {
+  //combines arrays horizontally
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (const l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+  // callback
+  // adds vertically
+  const verticalJoin = transpose(letters).map(ls => ls.join(''));
+  for (const l of verticalJoin) {
+    if (l.includes(word)) return true;
+  }
+  return false;
+};
+
+
+const transpose = function(matrix) {
+  let result = [];
+  for (let i = 0; i < matrix[0].length; i++) { // grab the length of the first row, it will give us column legnth since it's the same
+    let c = []; // here's where we will store our column values
+    for (let r = 0; r < matrix.length; r++) { // going through rows
+      c.push(matrix[r][i]); // push rows into column
+    }
+    result.push(c); // column into result
+  }
+  return result;
+};
+
+
+module.exports = wordSearch;
